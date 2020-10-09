7e23ceda28bf436e2f23ded7dad79e75e0fc2c4c
Edimar <edimarbmjunior@gmail.com>
2020-10-09 20:42:30 -0300

    commit inicial

---
+++ src/main/java/com/edimar/loja/LojaContrApplication.java
@@ -0,0 +1,12 @@
+package com.edimar.loja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaContrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaContrApplication.class, args);
+	}
+}
---
+++ src/test/java/com/edimar/loja/LojaContrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edimar.loja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaContrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
