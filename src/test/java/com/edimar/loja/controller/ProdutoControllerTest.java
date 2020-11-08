9735010e8c12032ff1aa96b9c81a462e112dfeff
Edimar <edimarbmjunior@gmail.com>
2020-11-08 20:21:11 -0300

    ajuste

--- src/test/java/com/edimar/loja/controller/ProdutoControllerTest.java
+++ src/test/java/com/edimar/loja/controller/ProdutoControllerTest.java
@@ -17,12 +17,14 @@ import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+
+import com.edimar.loja.services.ProdutoService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -35,6 +37,9 @@ public class ProdutoControllerTest {
 	private MockMvc mockMvc;
 	
 	private static String BASE_URL = "/produto";
+	
+	@MockBean
+	private ProdutoService produtoService;
 
 	@Before
 	public void setup() {
